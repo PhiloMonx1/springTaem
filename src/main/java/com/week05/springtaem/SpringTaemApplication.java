3a0107e04626a30f42f7df4000b7f544b86f7587
2m <philomonx1@gmail.com>
2022-08-06 19:26:01 +0900

    프로젝트 최초 세팅

---
+++ src/main/java/com/week05/springtaem/SpringTaemApplication.java
@@ -0,0 +1,13 @@
+package com.week05.springtaem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTaemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTaemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/week05/springtaem/SpringTaemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week05.springtaem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTaemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
