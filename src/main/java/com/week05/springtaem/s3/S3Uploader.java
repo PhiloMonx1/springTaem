567cb53de18d737db0525c371cacc411e2eca21a
2m <philomonx1@gmail.com>
2022-08-10 02:47:46 +0900

    [기록] S3 진행 중 폴더 없이

--- src/main/java/com/week05/springtaem/s3/S3Uploader.java
+++ src/main/java/com/week05/springtaem/s3/S3Uploader.java
@@ -33,7 +33,7 @@ public class S3Uploader {
 	}
 
 	private String upload(File uploadFile, String dirName) {
-		String fileName = dirName + "/" + UUID.randomUUID() + uploadFile.getName();
+		String fileName =UUID.randomUUID() + uploadFile.getName();
 		String uploadImageUrl = putS3(uploadFile, fileName);
 		removeNewFile(uploadFile);
 		return uploadImageUrl;
