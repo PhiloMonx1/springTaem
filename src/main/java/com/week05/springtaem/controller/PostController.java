f84e8b7ddb6344f4de5bfbf82ddf28400d1894be
2m <philomonx1@gmail.com>
2022-08-10 02:36:35 +0900

    [기록] S3 진행 중 폴더 붙이기

--- src/main/java/com/week05/springtaem/controller/PostController.java
+++ src/main/java/com/week05/springtaem/controller/PostController.java
@@ -4,6 +4,7 @@ import com.week05.springtaem.model.Post;
 import com.week05.springtaem.model.dto.PostRequestDto;
 import com.week05.springtaem.model.dto.PostResponseDto;
 import com.week05.springtaem.model.dto.UsernameDto;
+import com.week05.springtaem.s3.S3Uploader;
 import com.week05.springtaem.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,9 +17,13 @@ import java.util.List;
 @RequestMapping("/api")
 public class PostController {
 	private final PostService postService;
+	private final S3Uploader s3Uploader;
+
 	@Autowired
-	public PostController(PostService postService) {
+
+	public PostController(PostService postService, S3Uploader s3Uploader) {
 		this.postService = postService;
+		this.s3Uploader = s3Uploader;
 	}
 
 	@GetMapping("/posts")
@@ -34,6 +39,11 @@ public class PostController {
 	@PostMapping("/auth/post")
 	public Post creatPost(@RequestBody PostRequestDto postRequestDto){
 		return postService.creatPost(postRequestDto);
+	}
+
+	@PostMapping("/auth/post/image")
+	public String upload( MultipartFile multipartFile, String dirName) throws IOException {
+		return s3Uploader.upload(multipartFile, "post") + " 등록 완료.";
 	}
 
 	@PatchMapping("/auth/post/{postId}")
